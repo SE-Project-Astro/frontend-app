4c7ac5475392065488ed39e867db63ecbd22fb1d
Tharinda98 <tharindathamaranga98@gmail.com>
2021-01-22 00:16:01 +0530

    added validation to login page

--- src/views/examples/LoginPage.js
+++ src/views/examples/LoginPage.js
@@ -21,8 +21,71 @@ import{
 }from "reactstrap";
 
 export default function LoginPage(){
+    //form focusing states
     const [emailFocus, setEmailFocus] = React.useState(false);
     const [passwordFocus, setPasswordFocus] = React.useState(false);
+    //status for validation purposes
+    const[stateInput,setStateInput]=React.useState({input: {}});
+    const[stateError,setStateError]=React.useState({error: {}});
+
+    //functions for validation
+    const handleChange = (event)=>{
+        let input = stateInput.input;
+        input[event.target.name] =event.target.value;
+
+        setStateInput({
+            input: input
+        });
+    };
+
+    const handleSubmit =(event)=> {
+        event.preventDefault();
+        if(validate()){
+            console.log(stateInput);
+            let input = {};
+            input["Email"]="";
+            input["Password"]="";
+
+            setStateInput({
+                input: input
+            });
+            alert('login Successfull');
+        }
+    };
+
+    const validate = () =>{
+        let input=stateInput.input;
+        let error ={};
+        let isValid = true;
+
+        if (!input["Email"]){
+            isValid = false;
+            error["Email"] ="Please enter your email";
+        }
+
+        if (!input["Password"]){
+            isValid = false;
+            error["Password"]="Please enter your password";
+        }
+
+        if (typeof input["Email"] !== "undefined") {
+            var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+            if (!pattern.test(input["Email"])) {
+                isValid = false;
+                error["Email"] = "Please enter valid email address.";
+            }
+        }
+
+        setStateError({
+
+            error: error
+
+        });
+        return isValid;
+    }
+
+
+
 
 
     return(
@@ -45,7 +108,7 @@ export default function LoginPage(){
                                     </CardHeader>
                                     <br/>
                                     <CardBody>
-                                        <Form className="form">
+                                        <Form className="form" onSubmit={(e)=>handleSubmit(e)}>
                                             <InputGroup
                                                 className={classnames({
                                                     "input-group-focus": emailFocus,
@@ -60,11 +123,13 @@ export default function LoginPage(){
                                                     placeholder="Enter Email"
                                                     type="text"
                                                     name="Email"
-                                                    //onChange={(e) => handleChange(e)}
+                                                    value={stateInput.input.Email}
+                                                    onChange={(e) => handleChange(e)}
                                                     onFocus={(e) => setEmailFocus(true)}
                                                     onBlur={(e) => setEmailFocus(false)}
                                                 />
                                             </InputGroup>
+                                                <div>{stateError.error.Email}</div>
                                             <br/>
                                             <InputGroup
                                                 className={classnames({
@@ -80,11 +145,13 @@ export default function LoginPage(){
                                                     placeholder="Enter Password"
                                                     type="password"
                                                     name="Password"
-                                                    //onChange={(e) => handleChange(e)}
+                                                    value={stateInput.input.Password}
+                                                    onChange={(e) => handleChange(e)}
                                                     onFocus={(e) => setPasswordFocus(true)}
                                                     onBlur={(e) => setPasswordFocus(false)}
                                                 />
                                             </InputGroup>
+                                                <div>{stateError.error.Password}</div>
                                             <br/>
                                             <Button className="btn-round" color="success" size="lg">Log In</Button>
                                         </Form>
